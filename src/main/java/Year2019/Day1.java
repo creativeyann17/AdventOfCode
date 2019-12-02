8e2652547e96ed4746c45a55bed6e354a48f1296
Yann MARCOU <yann.marcou@bnc.ca>
2019-12-02 17:21:27 -0500

    Day1

---
+++ src/main/java/Year2019/Day1.java
@@ -0,0 +1,31 @@
+package Year2019;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class Day1 {
+  public static void main(String[]args) throws IOException {
+    Stream<String> stream = Files.lines(Paths.get("./data/input1.txt"));
+    BigDecimal totalMass = stream.map(mass ->
+        computeRequiredFuelForMass(mass)
+    ).reduce(BigDecimal.ZERO, BigDecimal::add);
+    System.out.println(totalMass);
+  }
+
+  public static BigDecimal computeRequiredFuelForMass(String mass) {
+    if (NumberUtils.isParsable(mass)) {
+      return new BigDecimal(mass)
+          .divide(new BigDecimal(3), 2, RoundingMode.HALF_EVEN)
+          .setScale(0, RoundingMode.DOWN)
+          .subtract(new BigDecimal(2));
+    } else {
+      return BigDecimal.ZERO;
+    }
+  }
+}

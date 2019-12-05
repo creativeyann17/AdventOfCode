5948ac44140680ca29483800b7d573ff7258a796
Yann MARCOU <yann.marcou@bnc.ca>
2019-12-05 15:46:36 -0500

    Day5 work in progress

--- src/main/java/year2019/Day5.java
+++ src/main/java/year2019/Day5.java
@@ -1,5 +1,66 @@
 package year2019;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Day5 {
+
+  public static final int OPERATION_EXIT = 99;
+  public static final int OPERATION_ADD = 1;
+  public static final int OPERATION_MULT = 2;
+  public static final int OPERATION_INPUT = 3;
+  public static final int OPERATION_OUTPUT = 4;
+
+  public static void main(String[] args) throws IOException {
+    String input = Utils.readFile("./data/input5.txt");
+    List<Integer> results = execute(parseInstructions(input));
+    System.out.println(results);
+  }
+
+  public static List<Integer> parseInstructions(String instructions) {
+    return Arrays.asList(instructions.split(",")).stream().map(instruction -> Integer.parseInt(instruction)).collect(Collectors.toList());
+  }
+
+  public static List<Integer> execute(List<Integer> instructions) {
+    List<Integer> instructionCopy = new ArrayList<>(instructions);
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < instructionCopy.size(); i += 4) {
+      int operation = instructionCopy.get(i);
+      if (operation == OPERATION_EXIT) {
+        break;
+      }
+
+      if (operation == OPERATION_INPUT) {
+        int operandResult = instructionCopy.get(i + 1);
+        System.out.print("Request input (int): ");
+        instructionCopy.set(operandResult, sc.nextInt());
+      } else if (operation == OPERATION_OUTPUT) {
+        int operandResult = instructionCopy.get(i + 1);
+        System.out.println(operandResult);
+      } else {
+
+        int operandLeft = instructionCopy.get(instructionCopy.get(i + 1));
+        int operandRight = instructionCopy.get(instructionCopy.get(i + 2));
+        int operandResult = instructionCopy.get(i + 3);
+
+        int result = 0;
+        switch (operation) {
+          case OPERATION_ADD:
+            result = operandLeft + operandRight;
+            break;
+          case OPERATION_MULT:
+            result = operandLeft * operandRight;
+            break;
+        }
+
+        instructionCopy.set(operandResult, result);
+      }
+    }
+    return instructionCopy;
+  }
 
 }
--- src/test/java/year2019/Day5Test.java
+++ src/test/java/year2019/Day5Test.java
@@ -1,11 +1,16 @@
 package year2019;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Day5Test {
 
   @Test
-  void test() {
-
+  void validateNewImplementation_workSameAsDay2() {
+    assertEquals(Arrays.asList(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50), Day5.execute(Arrays.asList(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)));
   }
+
 }

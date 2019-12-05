47daca7754abd79c57ca9ba7cce9d6182beb7485
Yann MARCOU <yann.marcou@bnc.ca>
2019-12-05 17:21:48 -0500

    Day5 almost ...

--- src/main/java/year2019/Day5.java
+++ src/main/java/year2019/Day5.java
@@ -29,38 +29,80 @@ public class Day5 {
     List<Integer> instructionCopy = new ArrayList<>(instructions);
     Scanner sc = new Scanner(System.in);
     for (int i = 0; i < instructionCopy.size(); i += 4) {
-      int operation = instructionCopy.get(i);
-      if (operation == OPERATION_EXIT) {
+      Parameter operation = Parameter.parse(instructionCopy.get(i));
+      if (operation.code == OPERATION_EXIT) {
         break;
       }
 
-      if (operation == OPERATION_INPUT) {
-        int operandResult = instructionCopy.get(i + 1);
-        System.out.print("Request input (int): ");
-        instructionCopy.set(operandResult, sc.nextInt());
-      } else if (operation == OPERATION_OUTPUT) {
-        int operandResult = instructionCopy.get(i + 1);
-        System.out.println(operandResult);
-      } else {
+      if (operation.code == OPERATION_INPUT) {
+        int operandResult = getValueByMode(instructionCopy, Parameter.MODE_IMMEDIATE, i + 1);
+        System.out.print("Input: ");
+        setValueWithPadding(instructionCopy, sc.nextInt(), operandResult);
+      } else if (operation.code == OPERATION_OUTPUT) {
+        int operandResult = getValueByMode(instructionCopy, Parameter.MODE_POSITION, i + 1);
+        System.out.println("Output: " + operandResult);
+      } else if (operation.code == OPERATION_ADD || operation.code == OPERATION_MULT) {
 
-        int operandLeft = instructionCopy.get(instructionCopy.get(i + 1));
-        int operandRight = instructionCopy.get(instructionCopy.get(i + 2));
-        int operandResult = instructionCopy.get(i + 3);
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        int operandResult = getValueByMode(instructionCopy, operation.param3Mode, i + 3);
 
         int result = 0;
-        switch (operation) {
+        switch (operation.code) {
           case OPERATION_ADD:
             result = operandLeft + operandRight;
+            System.out.println(String.format("%s + %s -> %s (mode: %s)", operandLeft, operandRight, operandResult, operation.param3Mode));
             break;
           case OPERATION_MULT:
             result = operandLeft * operandRight;
+            System.out.println(String.format("%s * %s -> %s (mode: %s)", operandLeft, operandRight, operandResult, operation.param3Mode));
             break;
+          default:
+            continue;
         }
 
-        instructionCopy.set(operandResult, result);
+        setValueWithPadding(instructionCopy, result, operandResult);
       }
     }
     return instructionCopy;
   }
 
+  private static void setValueWithPadding(List<Integer> instructions, int value, int index) {
+    for (int i = instructions.size(); i <= index; i++) {
+      instructions.add(0);
+    }
+    instructions.set(index, value);
+  }
+
+  private static int getValueByMode(List<Integer> instructions, int paramMode, int index) {
+    int value = instructions.get(index);
+    if (paramMode == Parameter.MODE_POSITION) {
+      value = instructions.get(value);
+    }
+    return value;
+  }
+
+  private static class Parameter {
+    public static final int MODE_POSITION = 0;
+    public static final int MODE_IMMEDIATE = 1;
+
+    public int code;
+    public int param1Mode = MODE_POSITION;
+    public int param2Mode = MODE_POSITION;
+    public int param3Mode = MODE_IMMEDIATE; // by default value store location is not by reference
+
+    public static Parameter parse(int value) {
+      Parameter param = new Parameter();
+      if (value < 99) {
+        param.code = value;
+      } else {
+        String valueStr = String.format("%05d", value);
+        param.code = Integer.parseInt(valueStr.substring(3, 5));
+        param.param1Mode = Integer.parseInt(String.valueOf(valueStr.charAt(2)));
+        param.param2Mode = Integer.parseInt(String.valueOf(valueStr.charAt(1)));
+        param.param3Mode = Integer.parseInt(String.valueOf(valueStr.charAt(0)));
+      }
+      return param;
+    }
+  }
 }
--- src/test/java/year2019/Day5Test.java
+++ src/test/java/year2019/Day5Test.java
@@ -2,6 +2,7 @@ package year2019;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,24 @@ import org.junit.jupiter.api.Test;
 class Day5Test {
 
   @Test
+  void Day2() throws IOException {
+    assertEquals(Arrays
+        .asList(4138687, 12, 2, 2, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 6, 1, 24, 1, 19, 10, 28, 2, 13, 23, 140, 1, 5, 27, 141, 2, 6, 31, 282, 1, 6, 35, 284,
+            2, 39, 9, 852, 1, 5, 43, 853, 1, 13, 47, 858, 1, 10, 51, 862, 2, 55, 10, 3448, 2, 10, 59, 13792, 1, 9, 63, 13795, 2, 67, 13, 68975, 1, 71, 6, 68977,
+            2, 6, 75, 137954, 1, 5, 79, 137955, 2, 83, 9, 413865, 1, 6, 87, 413867, 2, 91, 6, 827734, 1, 95, 6, 827736, 2, 99, 13, 4138680, 1, 6, 103, 4138682,
+            1, 2, 107, 4138684, 1, 111, 9, 0, 99, 2, 14, 0, 0), Day5.execute(Day5.parseInstructions(Utils.readFile("./data/input2.txt"))));
+  }
+
+
+  @Test
   void validateNewImplementation_workSameAsDay2() {
     assertEquals(Arrays.asList(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50), Day5.execute(Arrays.asList(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)));
+  }
+
+  @Test
+  void withParamMode() {
+    assertEquals(Arrays.asList(1002, 4, 3, 4, 33, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99),
+        Day5.execute(Arrays.asList(1002, 4, 3, 4, 33)));
   }
 
 }

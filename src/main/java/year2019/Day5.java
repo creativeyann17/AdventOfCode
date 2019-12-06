e3d84ae6d7f2dcfe50468b8159cad7977e121365
Yann MARCOU <yann.marcou@bnc.ca>
2019-12-06 12:31:32 -0500

    Day5 for the win

--- src/main/java/year2019/Day5.java
+++ src/main/java/year2019/Day5.java
@@ -14,11 +14,17 @@ public class Day5 {
   public static final int OPERATION_MULT = 2;
   public static final int OPERATION_INPUT = 3;
   public static final int OPERATION_OUTPUT = 4;
+  public static final int OPERATION_JUMP_IF_TRUE = 5;
+  public static final int OPERATION_JUMP_IF_FALSE = 6;
+  public static final int OPERATION_LESS_THAN = 7;
+  public static final int OPERATION_EQUALS = 8;
+
+  public static int INPUT = -1;
+  public static int OUTPUT = -1;
 
   public static void main(String[] args) throws IOException {
     String input = Utils.readFile("./data/input5.txt");
-    List<Integer> results = execute(parseInstructions(input));
-    System.out.println(results);
+    execute(parseInstructions(input));
   }
 
   public static List<Integer> parseInstructions(String instructions) {
@@ -28,8 +34,16 @@ public class Day5 {
   public static List<Integer> execute(List<Integer> instructions) {
     List<Integer> instructionCopy = new ArrayList<>(instructions);
     Scanner sc = new Scanner(System.in);
-    for (int i = 0; i < instructionCopy.size(); i += 4) {
+    int inc = 0;
+    for (int i = 0; i < instructionCopy.size(); i += inc) {
       Parameter operation = Parameter.parse(instructionCopy.get(i));
+
+      inc = 1;
+
+      if (!operation.isValid()) {
+        continue;
+      }
+
       if (operation.code == OPERATION_EXIT) {
         break;
       }
@@ -37,31 +51,51 @@ public class Day5 {
       if (operation.code == OPERATION_INPUT) {
         int operandResult = getValueByMode(instructionCopy, Parameter.MODE_IMMEDIATE, i + 1);
         System.out.print("Input: ");
-        setValueWithPadding(instructionCopy, sc.nextInt(), operandResult);
+        setValueWithPadding(instructionCopy, INPUT != -1 ? INPUT : sc.nextInt(), operandResult);
+        inc = 2;
       } else if (operation.code == OPERATION_OUTPUT) {
-        int operandResult = getValueByMode(instructionCopy, Parameter.MODE_POSITION, i + 1);
+        int operandResult = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
         System.out.println("Output: " + operandResult);
-      } else if (operation.code == OPERATION_ADD || operation.code == OPERATION_MULT) {
-
+        OUTPUT = operandResult;
+        inc = 2;
+      } else if (operation.code == OPERATION_ADD) {
         int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
         int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
         int operandResult = getValueByMode(instructionCopy, operation.param3Mode, i + 3);
-
-        int result = 0;
-        switch (operation.code) {
-          case OPERATION_ADD:
-            result = operandLeft + operandRight;
-            System.out.println(String.format("%s + %s -> %s (mode: %s)", operandLeft, operandRight, operandResult, operation.param3Mode));
-            break;
-          case OPERATION_MULT:
-            result = operandLeft * operandRight;
-            System.out.println(String.format("%s * %s -> %s (mode: %s)", operandLeft, operandRight, operandResult, operation.param3Mode));
-            break;
-          default:
-            continue;
+        setValueWithPadding(instructionCopy, operandLeft + operandRight, operandResult);
+        inc = 4;
+      } else if (operation.code == OPERATION_MULT) {
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        int operandResult = getValueByMode(instructionCopy, operation.param3Mode, i + 3);
+        setValueWithPadding(instructionCopy, operandLeft * operandRight, operandResult);
+        inc = 4;
+      } else if (operation.code == OPERATION_JUMP_IF_TRUE) {
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        if (operandLeft != 0) {
+          i = operandRight;
+          inc = 0;
         }
-
-        setValueWithPadding(instructionCopy, result, operandResult);
+      } else if (operation.code == OPERATION_JUMP_IF_FALSE) {
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        if (operandLeft == 0) {
+          i = operandRight;
+          inc = 0;
+        }
+      } else if (operation.code == OPERATION_LESS_THAN) {
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        int operandResult = getValueByMode(instructionCopy, operation.param3Mode, i + 3);
+        setValueWithPadding(instructionCopy, operandLeft < operandRight ? 1 : 0, operandResult);
+        inc = 4;
+      } else if (operation.code == OPERATION_EQUALS) {
+        int operandLeft = getValueByMode(instructionCopy, operation.param1Mode, i + 1);
+        int operandRight = getValueByMode(instructionCopy, operation.param2Mode, i + 2);
+        int operandResult = getValueByMode(instructionCopy, operation.param3Mode, i + 3);
+        setValueWithPadding(instructionCopy, operandLeft == operandRight ? 1 : 0, operandResult);
+        inc = 4;
       }
     }
     return instructionCopy;
@@ -89,20 +123,27 @@ public class Day5 {
     public int code;
     public int param1Mode = MODE_POSITION;
     public int param2Mode = MODE_POSITION;
-    public int param3Mode = MODE_IMMEDIATE; // by default value store location is not by reference
+    public int param3Mode = MODE_IMMEDIATE;
 
     public static Parameter parse(int value) {
       Parameter param = new Parameter();
-      if (value < 99) {
+
+      if (value <= 99) {
         param.code = value;
       } else {
         String valueStr = String.format("%05d", value);
+        // System.out.println(valueStr);
         param.code = Integer.parseInt(valueStr.substring(3, 5));
         param.param1Mode = Integer.parseInt(String.valueOf(valueStr.charAt(2)));
         param.param2Mode = Integer.parseInt(String.valueOf(valueStr.charAt(1)));
-        param.param3Mode = Integer.parseInt(String.valueOf(valueStr.charAt(0)));
+        // param.param3Mode = Integer.parseInt(String.valueOf(valueStr.charAt(0)));
       }
       return param;
     }
+
+    public boolean isValid() {
+      return (param1Mode == 0 || param1Mode == 1) && (param2Mode == 0 || param2Mode == 1) && (param2Mode == 0 || param2Mode == 1);
+    }
   }
+
 }
--- src/test/java/year2019/Day5Test.java
+++ src/test/java/year2019/Day5Test.java
@@ -5,9 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class Day5Test {
+
+  @Before
+  void before() {
+    Day5.INPUT = 0;
+  }
 
   @Test
   void Day2() throws IOException {
@@ -18,6 +24,17 @@ class Day5Test {
             1, 2, 107, 4138684, 1, 111, 9, 0, 99, 2, 14, 0, 0), Day5.execute(Day5.parseInstructions(Utils.readFile("./data/input2.txt"))));
   }
 
+  @Test
+  void input_output() throws IOException {
+    Day5.INPUT = 1;
+    assertEquals(Arrays.asList(1, 0, 4, 0, 99), Day5.execute(Arrays.asList(3, 0, 4, 0, 99)));
+  }
+
+  @Test
+  void negative() throws IOException {
+    assertEquals(Arrays.asList(1101, 100, -1, 4, 99), Day5.execute(Arrays.asList(1101, 100, -1, 4, 0)));
+  }
+
 
   @Test
   void validateNewImplementation_workSameAsDay2() {
@@ -26,8 +43,111 @@ class Day5Test {
 
   @Test
   void withParamMode() {
-    assertEquals(Arrays.asList(1002, 4, 3, 4, 33, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99),
-        Day5.execute(Arrays.asList(1002, 4, 3, 4, 33)));
+    assertEquals(Arrays.asList(1002, 4, 3, 4, 99), Day5.execute(Arrays.asList(1002, 4, 3, 4, 33)));
   }
+
+  @Test
+  void equals8_positionMode() {
+    Day5.INPUT = 8;
+    assertEquals(Arrays.asList(3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8), Day5.execute(Arrays.asList(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 7;
+    assertEquals(Arrays.asList(3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8), Day5.execute(Arrays.asList(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void lessThan8_positionMode() {
+    Day5.INPUT = 7;
+    assertEquals(Arrays.asList(3, 9, 7, 9, 10, 9, 4, 9, 99, 1, 8), Day5.execute(Arrays.asList(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 9;
+    assertEquals(Arrays.asList(3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8), Day5.execute(Arrays.asList(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void equals8_immediateMode() {
+    Day5.INPUT = 8;
+    assertEquals(Arrays.asList(3, 3, 1108, 1, 8, 3, 4, 3, 99), Day5.execute(Arrays.asList(3, 3, 1108, -1, 8, 3, 4, 3, 99)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 9;
+    assertEquals(Arrays.asList(3, 3, 1108, 0, 8, 3, 4, 3, 99), Day5.execute(Arrays.asList(3, 3, 1108, -1, 8, 3, 4, 3, 99)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void lessThan8_immediateMode() {
+    Day5.INPUT = 7;
+    assertEquals(Arrays.asList(3, 3, 1107, 1, 8, 3, 4, 3, 99), Day5.execute(Arrays.asList(3, 3, 1107, -1, 8, 3, 4, 3, 99)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 9;
+    assertEquals(Arrays.asList(3, 3, 1107, 0, 8, 3, 4, 3, 99), Day5.execute(Arrays.asList(3, 3, 1107, -1, 8, 3, 4, 3, 99)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void jump_positionMode() {
+    Day5.INPUT = 2;
+    assertEquals(Arrays.asList(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 2, 1, 1, 9),
+        Day5.execute(Arrays.asList(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 0;
+    assertEquals(Arrays.asList(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 0, 0, 1, 9),
+        Day5.execute(Arrays.asList(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void jump_immediateMode() {
+    Day5.INPUT = 2;
+    assertEquals(Arrays.asList(3, 3, 1105, 2, 9, 1101, 0, 0, 12, 4, 12, 99, 1), Day5.execute(Arrays.asList(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1)));
+    assertEquals(1, Day5.OUTPUT);
+
+    Day5.INPUT = 0;
+    assertEquals(Arrays.asList(3, 3, 1105, 0, 9, 1101, 0, 0, 12, 4, 12, 99, 0), Day5.execute(Arrays.asList(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1)));
+    assertEquals(0, Day5.OUTPUT);
+  }
+
+  @Test
+  void big_example_equals8() {
+    Day5.INPUT = 8;
+    assertEquals(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 1000, 8, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99), Day5.execute(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99)));
+    assertEquals(1000, Day5.OUTPUT);
+  }
+
+  @Test
+  void big_example_below8() {
+    Day5.INPUT = 7;
+    assertEquals(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 7, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99), Day5.execute(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99)));
+    assertEquals(999, Day5.OUTPUT);
+  }
+
+
+  @Test
+  void big_example_bigger8() {
+    Day5.INPUT = 9;
+    assertEquals(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 1001, 9, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99), Day5.execute(Arrays
+        .asList(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999,
+            1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99)));
+    assertEquals(1001, Day5.OUTPUT);
+  }
+
+
 
 }
